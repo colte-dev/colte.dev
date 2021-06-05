de93b1d81dfce3fc42907faf984808f75127df24
VincentJonathan <vinjongm@gmail.com>
2021-06-05 11:40:34 +0700

    add dense variant

--- client/src/components/Author.svelte
+++ client/src/components/Author.svelte
@@ -4,16 +4,18 @@
 	export let name: string;
 	export let avatarUrl: string;
 	export let label: string;
+	export let dense = false;
+
+	const size = !dense ? 44 : 32;
+	const nameClasses = dense ? "inline-block mr-1 text-md" : "text-lg";
 </script>
 
 <div on:click={() => goto("/" + name)} class="cursor-pointer">
 	<div class="flex items-center ">
-		<img class="rounded-full" width="44" height="44" src={avatarUrl} alt="avatar" />
+		<img class="rounded-full" width={size} height={size} src={avatarUrl} alt="avatar" />
 		<div class="pl-4 py-2">
-			<div class="text-lg text-gray-300">{name}</div>
-			<div class="text-xs text-gray-400 ">
-				{label}
-			</div>
+			<div class={`text-gray-300 ${nameClasses}`}>{name}</div>
+			<div class={`text-xs text-gray-400 ${dense ? "inline-block" : ""}`}>{label}</div>
 		</div>
 	</div>
 </div>
